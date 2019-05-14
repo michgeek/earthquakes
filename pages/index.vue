d776b0dca7b9391aead5af55e4d58e89845e1db6
Michgeek <mmichmich@gmail.com>
2019-05-14 22:02:43 +1000

    Add info window

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
     <GmapMap :center="{lat:10, lng:10}" :zoom="2" map-type-id="terrain" style="width: 100vw; height: 100vh">
-        <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position"/>
+        <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" v-on:click="selected = m"/>
+        <gmap-info-window v-if="selected"
+                          :options="{pixelOffset: {width: 0,height: -35}}"
+                          :position="selected.position"
+                          :opened="selected ? true : false" v-on:closeclick="selected=null">
+            <h1>{{ selected.title }}</h1>
+            <ul>
+                <li>Place: {{ selected.place }}</li>
+                <li>Time: {{ selected.time }} TZ: {{ selected.tz }}</li>
+                <li>Magnitude: {{ 1.4 }}</li>
+                <li><a :href="selected.url">More details...</a></li>
+            </ul>
+        </gmap-info-window>
     </GmapMap>
 </template>
 <script>
@@ -8,7 +20,10 @@
 
     export default {
         data() {
-            return {feed};
+            return {
+                feed,
+                selected: null
+            };
         },
         computed: {
             earthquakes() {
@@ -17,10 +32,10 @@
             markers() {
                 return this.earthquakes.map(({properties, geometry}) => {
                     return {
-                        title: `${properties.title} - ${properties.time}`,
+                        ...properties,
                         position: {
-                            lat: geometry.coordinates[0],
-                            lng: geometry.coordinates[1]
+                            lng: geometry.coordinates[0],
+                            lat: geometry.coordinates[1],
                         }
                     };
                 });
